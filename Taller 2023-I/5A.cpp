c07e6fc9410b113c3d4a0e6327584d3b1d448ece
franbelen <francisca.aguayo@uc.cl>
2023-04-25 15:08:57 -0400

    avance semana 5

---
+++ Taller 2023-I/5A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> V, N;
+int S, b, Times;
+int M =10;
+
+vector<int> recursion(vector<int> arr, int i) {
+    if (i == M){
+        return arr;
+    }
+}
+
+int main()
+{
+    cin >> Times;
+
+    # repetición
+    N.clear(), V.clear();
+    for (int j = 10; j>0 , j--)
+        N.push_back(j);
+    
+    for (int i = 0; i < M; i++)
+    {
+        cin >> b;
+        V.push_back(b);
+    }
+
+}
